52bedb1b32a3992de6e2053acc0f97a1e5f7f743
Taha Parsayan <parsayan.t@gmail.com>
2025-06-19 14:47:17 +0400

    Update Try.py

--- Langchain/Try.py
+++ Langchain/Try.py
@@ -11,10 +11,13 @@ sys.path.append(parent_path)
 from Update_Git import git_add, git_commit, git_push
 
 # Update Git Repository
-file_path = os.path.join(current_path, "Try.py")
-git_add(file_path)
-git_commit("Update Try.py")
-git_push("main")
+try:
+    file_path = os.path.join(current_path, "Try.py")
+    git_add(file_path)
+    git_commit("Update Try.py")
+    git_push("main")
+except Exception as e:
+    print(f"An error occurred while updating the git repository\n: {e}")
 
 # Get the document from webpage and split it into chunks
 def document_loader(url):
