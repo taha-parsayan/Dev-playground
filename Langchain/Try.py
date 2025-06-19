c1e351d5c1c859f04434d9d9bc5c33bd7f02ab8e
Taha Parsayan <parsayan.t@gmail.com>
2025-06-19 14:39:07 +0400

    Update Try.py

--- Langchain/Try.py
+++ Langchain/Try.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from langchain_community import document_loaders
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 # Add the parent directory to the system path
 current_path = os.getcwd()
 parent_path = os.path.abspath(os.path.join(current_path, ".."))
@@ -11,3 +15,31 @@ file_path = os.path.join(current_path, "Try.py")
 git_add(file_path)
 git_commit("Update Try.py")
 git_push("main")
+
+# Get the document from webpage and split it into chunks
+def document_loader(url):
+
+    # Load documents from a given URL
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+
+    # Split the documents into chunks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 50,
+    )
+
+    split_docs = splitter.split_documents(docs)
+
+    return split_docs
+
+def create_db(docs):
+    pass
+
+def create_chain():
+    pass
+
+
+# Main
+if __name__ == "__main__":
+    pass
