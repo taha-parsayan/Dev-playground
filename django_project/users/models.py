c339b54e487353cf56d114fc4ed991c60473c41e
Taha Parsayan <parsayan.t@gmail.com>
2026-02-11 07:31:14 +0100

    y

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+# dunder method to return the username when we print the profile object
+    def __str__(self):
+        return f'{self.user.username} Profile'
